82cb4cecae85b5fe297b4734c529b8343d157260
radu.besliu <radu.besliu@atta.systems>
2023-02-27 17:49:12 +0200

    Laborator 2

---
+++ lab/src/main/java/lab1/Exemplu2.java
@@ -0,0 +1,16 @@
+package main.java.lab1;
+public class Exemplu2 {
+    public static void main(String[] args) {
+        Exemplu1 e1 = new Exemplu1();
+        e1.setNumarLaborator(1);
+        System.out.println(e1.getNumarLaborator());
+        System.out.println(e1.numarLaborator);
+
+        Exemplu1 e2 = new Exemplu1();
+        e2.setNumarLaborator(2);
+        System.out.println(e2.getNumarLaborator());
+
+        Exemplu1 e3 = new Exemplu1();
+        System.out.println(e3.numarLaborator);
+    }
+}
---
+++ lab/src/main/java/lab1/Exemplu4.java
@@ -0,0 +1,11 @@
+package main.java.lab1;
+
+public class Exemplu4 {
+    public static void main(String[] args) {
+        System.out.println(5 % 1);
+
+        int a = 5;
+        int b = 6;
+        System.out.println(a == b);
+    }
+}
